e66879d28ed52b0bf27b5c74b5f711e393c7452a
InfinityDeer <infinitydeer@outlook.com>
2016-06-13 12:00:46 -0400

    ~Removed Unity Project Folder

--- Unity Project/Assets/EasyRoads3D Free/Editor/TerrainEditorScript.js
+++
@@ -1,9 +0,0 @@
-@CustomEditor(EasyRoads3DTerrainID)
-class TerrainEditorScript extends Editor
-{
-function OnSceneGUI()
-{
-if(Event.current.shift && RoadObjectScript.OCCDOQCCQQ != null) Selection.activeGameObject = RoadObjectScript.OCCDOQCCQQ.gameObject;
-else RoadObjectScript.OCCDOQCCQQ = null;
-}
-}
